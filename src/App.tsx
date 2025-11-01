344608d6f98152954dd742f3259bed26b3eab499
Bigdreams415 <joshuaokoghie111@gmail.com>
2025-11-01 12:41:06 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        POS Inventory System 🎉
+      </h1>
+      <p className="text-lg text-gray-600 mt-4">
+        Electron + React + TypeScript + Tailwind
+      </p>
+    </div>
+  );
+}
+
+export default App;
